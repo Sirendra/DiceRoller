95faafa9412751638a71b30a68779cf171a6655d
jerry <Sirendra99@gmail.com>
2021-10-06 12:30:51 +0700

    Intial Commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            rollDice()
+        }
+        rollDice()
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6) //Object creation of the class Dice; Note we didn't use the keyword "new"
+        val diceRoll = dice.roll()
+        var result=when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        imageView.setImageResource(result)
+        imageView.contentDescription = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
+
